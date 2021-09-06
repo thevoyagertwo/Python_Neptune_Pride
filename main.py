faeedc312a7fc8c6d9a3a2330a9487226b80ba00
thevoyagertwo <thevoyagertwo@protonmail.com>
2021-09-06 10:03:56 +0100

    Cleaned up printing and added % increase for ship output

--- main.py
+++ main.py
@@ -4,16 +4,26 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import math
 
+def combat(my_wep,enemy_wep,my_ships,enemy_ships):
+    return
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    industry = 119
-    science_required = 1152 - 34
-    science_start = 51
-    science_end   = 75
+    industry = 241
+    manu_start = 5
+    manu_end   = 15
+    science_required = 1440-14-83-83
+    science_required = 1152 - 32
+    science_start = 60
+    science_end   = 125
 
-    for manutech in range(0, 10):
-        print('Manu Tech: ' + str(manutech) + ' ships per hour = ' + str( industry * (manutech + 5) / 24) )
+    ships_per_hour = round(industry * (manu_start - 1 + 5) / 24, 2) ## starting manutech - 1 calc
+    for manutech in range(manu_start, manu_end):
+        increase = round(industry * (manutech + 5) / 24,2)/ships_per_hour * 100
+        increase = round(increase,1)
+        print('Manu Tech: ' + str(manutech) + ' ships per hour = ' + str( ships_per_hour) + '      increase % = ' + str(increase))
+        ships_per_hour = round(industry * (manutech + 5) / 24,2)
 
     round_time = 999 # just to start to compare to
     for science in range(science_start,science_end):
